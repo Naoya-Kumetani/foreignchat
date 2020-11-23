b85608402a84afd7fc22dccec2facc66532ea6a1
Naoya-Kumetani <mrkume38@keio.jp>
2020-11-23 20:41:05 +0900

    チャットコントローラー等

--- backend/database/migrations/2020_11_07_161508_create_chats_table.php
+++ backend/database/migrations/2020_11_07_161508_create_chats_table.php
@@ -14,8 +14,13 @@ class CreateChatsTable extends Migration
     public function up()
     {
         Schema::create('chats', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('from');
+            $table->unsignedBigInteger('to');
+            $table->text('body',1000);
             $table->timestamps();
+            $table->foreign('from')->references('id')->on('menbers');
+            $table->foreign('to')->references('id')->on('menbers');
         });
     }
 
